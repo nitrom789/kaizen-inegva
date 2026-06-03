e403140e0914b24e38c1bfb2a341f72bbaec39c5
Nikolay <nitrom@ukr.net>
2026-06-03 19:41:42 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,63 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono } from "next/font/google";
 
-import "./globals.css"
+import "./globals.css";
+
 import { Toaster } from "sonner";
-import { ThemeProvider } from "@/components/theme-provider"
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+
+
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
+
   return (
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable
+      )}
     >
-      <body>
-       {children}
 
-       <Toaster
-          position="top-right"
-          richColors
-        />
+      <body>
+
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+
+          {children}
+
+            <Toaster
+              position="top-right"
+              richColors
+            />
+
+        </ThemeProvider>
+
       </body>
+
     </html>
-  )
+  );
 }
